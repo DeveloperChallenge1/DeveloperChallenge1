ff732a2ec8f1cd9ca433b6ed1c5c729fc7377227
DeveloperChallenge1 <r@debian>
2018-06-03 13:27:10 +0200

    index.ja should connect to mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // process.env
 
 var restify = require('restify');
+var mongoose = requiere('mongoose/');
 
 function respond(req, res, next) {
   //res.send('hello ' + req.params.name);
@@ -14,6 +15,8 @@ function respond(req, res, next) {
 var server = restify.createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
+
+mongoose.connect('mongodb://heroku_frbg5gzs:mgrdlv5rqdnbin1l7vupiig9iv@ds255588.mlab.com:55588/heroku_frbg5gzs');
 
 server.listen(process.env.PORT, function() {
   console.log('%s listening at %s', server.name, server.url);
